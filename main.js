a3bfe06ca86503b103452892742cc97810ff0c2d
asiyebulut <atubakesenel@gmail.com>
2021-03-02 13:20:44 -0800

    you can calculate your age now

---
+++ main.js
@@ -0,0 +1,8 @@
+var currentYear = 2021;
+console.log(currentYear);
+var birthYear;
+birthYear = 1991;
+console.log(birthYear);
+var age = currentYear - birthYear;
+var altAge = currentYear - birthYear - 1;  //was not sure about what means second age!
+console.log('They are either ' + age + ' or ' + altAge + '.');
